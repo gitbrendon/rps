431f9306e6b8fb508067fb3f9b2a3f483cfaa509
gitbrendon <brendon.cloyd@atonomi.io>
2019-01-18 13:27:37 -0800

    changeOwner() uses onlyOwner modifier, checks for 0 address

--- contracts/Owned.sol
+++ contracts/Owned.sol
@@ -3,24 +3,24 @@ pragma solidity 0.5;
 contract Owned {
     address private owner;
 
-    event LogOwnerChanged(address indexed previousOwner, address newOwner);
+    event LogOwnerChanged(address indexed previousOwner, address indexed newOwner);
 
     constructor() public {
         owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Must be contract owner");
+        _;
     }
 
     function getOwner() public view returns(address) {
         return owner;
     }
 
-    function changeOwner(address newOwner) public {
-        require(msg.sender == owner, "Must be contract owner");
+    function changeOwner(address newOwner) public onlyOwner {
+        require(newOwner != address(0), "Must provide address for new owner");
         emit LogOwnerChanged(owner, newOwner);
         owner = newOwner;
-    }
-
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Must be contract owner");
-        _;
     }
 }
